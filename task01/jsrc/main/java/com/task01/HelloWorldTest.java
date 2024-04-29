1e0b0da55832021762d1558a8e625e4a45d8b16a
Prashant Mangate <prashant_mangate@epam.com>
2024-04-29 16:58:28 -0700

    updated code

--- task01/jsrc/main/java/com/task01/HelloWorldTest.java
+++ task01/jsrc/main/java/com/task01/HelloWorldTest.java
@@ -1,4 +1,4 @@
-package com.DemoSyndicateJava;
+package com.task01;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -8,13 +8,13 @@ import com.syndicate.deployment.model.RetentionSetting;
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
+@LambdaHandler(lambdaName = "hello_world-test",
+	roleName = "hello_world-test-role",
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorldTest implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
