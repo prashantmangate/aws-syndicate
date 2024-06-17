dac7fd39ceee04874e01ff4d99ce2726b93f2cd6
Prashant Mangate <prashant_mangate@epam.com>
2024-06-16 23:49:17 -0700

    add project task05

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -57,7 +57,7 @@ private AmazonDynamoDB amazonDynamoDB;
 			}
 			contentString.append("'}'");
 			AttributeValue contentStr = new AttributeValue();
-			contentStr.setS(contentString.toString());
+			contentStr.setS("any map '{' content '}'");
 
 			resultMap.put("id",  new AttributeValue(id));	
 			resultMap.put("principalId",pId);
