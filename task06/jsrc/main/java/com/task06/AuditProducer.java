f8fcfe445c6d79e78ab7fd195d91f75dc0c97797
Prashant Mangate <prashant_mangate@epam.com>
2024-06-18 18:05:58 -0700

    updated code

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -43,74 +43,44 @@ public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
 	@Override
 	public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
 
-		       System.out.println("Inside handleRequest and received dynamoDBEvent " + dynamodbEvent);
+	   System.out.println("Inside handleRequest and received dynamoDBEvent " + dynamodbEvent);
        for (DynamodbEvent.DynamodbStreamRecord record : dynamodbEvent.getRecords()) {
           // Check if it's an updated record
           System.out.println("Iterating event records " + record);
+          System.out.println("Event Name is Modify");
+          // Extract the updated record
+          // Here you'll have access to the new and old images of the record
+          // Perform necessary processing on the updated record
+          // Example: Accessing the new image
+          Map<String, AttributeValue> newImage = record.getDynamodb().getNewImage();
+          System.out.println("New Updated Object " + newImage);
+          // Example: Accessing the old image
+          Map<String, AttributeValue> oldImage = record.getDynamodb().getOldImage();
+          if(oldImage!=null)
+             System.out.println("Old Object "+oldImage );
+          // Add logic to process the updated record and add it to the new table
+          // AddNewRecordToNewTable(newImage);
+
+          Table table = DYNAMO_DB.getTable(TABLE_AUDIT);
+          System.out.println("table name Object " + table.getTableName());
+          // Create a map of attributes for the item
+          String id = java.util.UUID.randomUUID().toString();
+          String modificationDate = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+
           if (record.getEventName().equals("MODIFY")) {
-             System.out.println("Event Name is Modify");
-             // Extract the updated record
-             // Here you'll have access to the new and old images of the record
-             // Perform necessary processing on the updated record
-             // Example: Accessing the new image
-             Map<String, AttributeValue> newImage = record.getDynamodb().getNewImage();//.getDynamodb().getNewImage();
-             System.out.println("New Updated Object " + newImage);
-             // Example: Accessing the old image
-             Map<String, AttributeValue> oldImage = record.getDynamodb().getOldImage();
-             System.out.println("Old Object " + oldImage);
-             System.out.println("Should be printed");
-             // Add logic to process the updated record and add it to the new table
-             // AddNewRecordToNewTable(newImage);
-             System.out.println("Should be printed 2");
-             Table table = DYNAMO_DB.getTable(TABLE_AUDIT);
-             System.out.println("table name Object " + table.getTableName());
-             // Create a map of attributes for the item
-             String id = java.util.UUID.randomUUID().toString();
-             String modificationDate = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
-             if(oldImage.isEmpty()){
-                Map<String,String> newKeyMap = new HashMap<>();
-                newKeyMap.put("key", String.valueOf(newImage.get("key")));
-                newKeyMap.put("value", String.valueOf(newImage.get("value")));
-                System.out.println("newKeyMap " + newKeyMap);
-                System.out.println("modificationDate " + modificationDate);
-                System.out.println("itemKey " + newImage.get("key"));
-                System.out.println("id " + id);
-                PutItemOutcome outcome = table.putItem(new Item()
-                      .withPrimaryKey("id", id)
-                      .with("modificationDate", modificationDate)
-                      .with("itemKey", newImage.get("key"))
-                      .with("newValue", newKeyMap));
-             }else {
-                System.out.println("oldValue " + oldImage.get("value"));
-                System.out.println("newValue " + newImage.get("value"));
-                System.out.println("modificationDate " + modificationDate);
-                System.out.println("itemKey " + newImage.get("key"));
-                System.out.println("id " + id);
-                PutItemOutcome outcome = table.putItem(new Item()
-                      .withPrimaryKey("id", id)
-                      .with("modificationDate", modificationDate)
-                      .with("itemKey", newImage.get("key"))
-                      .with("updatedAttribute", "value")
-                      .with("oldValue", oldImage.get("value"))
-                      .with("newValue", newImage.get("value")));
-             }
+               if(oldImage==null) {
+                  this.addItemtoAuditOnInsertEvent(table,id, newImage, modificationDate);
+		   	   } else if(oldImage.isEmpty()) {
+                  this.addItemtoAuditOnInsertEvent(table,id, newImage, modificationDate);
+               } else {
+                this.addItemtoAuditOnUpdateEvent(table,id, newImage,oldImage, modificationDate);
+               }
           } else if (record.getEventName().equals("INSERT")) {
-             System.out.println("Event Name is Modify");
-             // Example: Accessing the new image
-             Map<String, AttributeValue> newImage = record.getDynamodb().getNewImage();
-             System.out.println("New Updated Object " + newImage);
-             // Example: Accessing the old image
-             Map<String, AttributeValue> oldImage = record.getDynamodb().getOldImage();
-             System.out.println("Old Object " + oldImage);
+             System.out.println("Event Name is INSERT");
+             this.addItemtoAuditOnInsertEvent(table,id, newImage, modificationDate);
           } else if (record.getEventName().equals("REMOVE")) {
-             // Process removed records
-             System.out.println("Event Name is Remove");
-             // Example: Accessing the new image
-             Map<String, AttributeValue> newImage = record.getDynamodb().getNewImage();
-             System.out.println("New Updated Object " + newImage);
-             // Example: Accessing the old image
-             Map<String, AttributeValue> oldImage = record.getDynamodb().getOldImage();
              System.out.println("Old Object " + oldImage);
+             this.addItemtoAuditOnUpdateEvent(table,id, newImage,oldImage, modificationDate);
           } else {
              System.out.println("Event Name is Unknown " + record.getEventName() );
              // Handle other types of events, if necessary
@@ -119,5 +89,24 @@ public class AuditProducer implements RequestHandler<DynamodbEvent, Void> {
 	return null;
 
 	}
+   private void addItemtoAuditOnUpdateEvent(Table table,String id, Map<String, AttributeValue> newImage,Map<String, AttributeValue> oldImage, String modificationDate){
+                PutItemOutcome outcome = table.putItem(new Item()
+                      .withPrimaryKey("id", id)
+                      .with("modificationDate", modificationDate)
+                      .with("itemKey", newImage.get("key").toString())
+                      .with("updatedAttribute", "value")
+                      .with("oldValue", oldImage.get("value").toString())
+                      .with("newValue", newImage.get("value").toString()));
+   }
 
+   private void addItemtoAuditOnInsertEvent(Table table,String id, Map<String, AttributeValue> newImage, String modificationDate){
+      Map<String,String> newKeyMap = new HashMap<>();
+      newKeyMap.put("key", String.valueOf(newImage.get("key")));
+      newKeyMap.put("value", String.valueOf(newImage.get("value")));
+      PutItemOutcome outcome = table.putItem(new Item()
+            .withPrimaryKey("id", id)
+            .with("modificationDate", modificationDate)
+            .with("itemKey", newImage.get("key").toString())
+            .with("newValue", newKeyMap));
+   }
 }
