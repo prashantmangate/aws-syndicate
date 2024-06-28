44c118ccf72462be732de15bd4dc5a6cc43e43bc
Prashant Mangate <prashant_mangate@epam.com>
2024-06-28 00:37:00 -0700

    add code for task08

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,49 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.weather.report.HttpURLConnectionExample;
+
+import java.io.IOException;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "api_handler",
+	roleName = "api_handler-role",
+	isPublishVersion = false,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+
+@LambdaLayer(layerName = "WeatherReportApi",
+		libraries = {"/java/lib/commons-lang3-3.14.0.jar", "/java/lib/gson-2.10.1.jar","/java/lib/WeatherReportApi-1.0-SNAPSHOT.jar"},
+		runtime = DeploymentRuntime.JAVA11,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP)
+
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
+
+	private final Gson gson = new Gson();
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		Map response = null;
+		try {
+			Gson gson = new Gson();
+			 response = gson.fromJson(HttpURLConnectionExample.sendGET(), Map.class);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return response;
+	}
+}
