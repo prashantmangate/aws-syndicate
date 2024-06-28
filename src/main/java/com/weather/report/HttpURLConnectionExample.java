3949d32fed6a1e03ed4ea2772a6fdcd7b8aeacaf
Prashant Mangate <prashant_mangate@epam.com>
2024-06-28 14:24:23 -0700

    add weather report api invoke program

---
+++ src/main/java/com/weather/report/HttpURLConnectionExample.java
@@ -0,0 +1,45 @@
+package com.weather.report;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpURLConnectionExample {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+
+    private static final String GET_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+    public static void main(String[] args) throws IOException {
+        sendGET();
+        System.out.println("GET DONE");
+    }
+
+    private static void sendGET() throws IOException {
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request did not work.");
+        }
+
+    }
+
+}
