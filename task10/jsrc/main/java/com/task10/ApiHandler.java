500c2235ba52712f09a7e7fb93f19de39488505e
Prashant Mangate <prashant_mangate@epam.com>
2024-07-02 15:13:29 -0700

    updated code

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -1,15 +1,36 @@
 package com.task10;
 
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.print.DocFlavor.READER;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.stream.JsonReader;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminCreateUserRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.MessageActionType;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.UserPoolDescriptionType;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminCreateUserRequest.Builder;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.CognitoIdentityProviderException;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolClientsRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsResponse;
 
 @LambdaHandler(lambdaName = "api_handler",
 	roleName = "api_handler-role",
@@ -20,13 +41,21 @@ import com.syndicate.deployment.model.RetentionSetting;
 @EnvironmentVariables(value = {
         @EnvironmentVariable(key = "region", value = "${region}"),
         @EnvironmentVariable(key = "tables_table", value = "${tables_table}"),
-		@EnvironmentVariable(key = "reservations_table", value = "${tables_table}"),
-		@EnvironmentVariable(key = "booking_userpool", value = "${tables_table}")
+		@EnvironmentVariable(key = "reservations_table", value = "${reservations_table}"),
+		@EnvironmentVariable(key = "booking_userpool", value = "${booking_userpool}")
 	})
 public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
+	private static final String USER_POOL = System.getenv("booking_userpool");
+	private static final Region REGION = Region.EU_CENTRAL_1;
+	private final Gson gson = new Gson();
+
+	private CognitoIdentityProviderClient identityProviderClient;
+
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		
+		identityProviderClient = CognitoIdentityProviderClient.builder().region(REGION).build();
+
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		
 		String rawPath = "";
@@ -42,23 +71,33 @@ public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 		/reservations GET
 		*/
 
-		if(reqObj.get("rawPath") instanceof String)
-			rawPath = reqObj.get("rawPath").toString();
+		if(reqObj.get("resource") instanceof String)
+			rawPath = reqObj.get("resource").toString();
 
-		if(rawPath.contains("singup")) {// POST
+		if(rawPath.contains("/signup")) {// POST
+
 			System.out.println("resource name: "+rawPath);
-			// POST request will contain:
-			// {
-			// 	"firstName": // string
-			// 	"lastName": // string
-			// 	"email": // email validation
-			// 	"password": // alphanumeric + any of "$%^*", 12+ chars
-			// }
+			 Builder createUserRequest = AdminCreateUserRequest.builder().messageAction(MessageActionType.SUPPRESS);
+			
+			 // createUserRequest
+			// .username(reqObj.get("email").toString())
+			// .temporaryPassword("Aws1#AwsCloud2#")
+			// .userPoolId(userPoolId);
+			System.out.println(reqObj.get("body").toString());
 
-			resultMap.put("statusCode", 200);
-			// resultMap.put("body", "{" +
-			// 		"    \"statusCode\": 200" +
-			// 		"}");
+			JsonReader reader = new JsonReader(new StringReader(reqObj.get("body").toString()));
+			reader.setLenient(true);
+			User user = gson.fromJson(reader, User.class);
+			System.out.println(user.toString());
+			signUp(identityProviderClient,  
+					getClientId(), 
+					user.getFirstName(), 
+					user.getLastName(), 
+					user.getEmail(), 
+					user.getPassword());
+
+			identityProviderClient.close();
+			resultMap.put("statusCode", 200);			
 		}
 		else if(rawPath.contains("singin")) { // POST
 			System.out.println("resource name: "+rawPath);
@@ -167,4 +206,64 @@ public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 		return resultMap;
 
 	}
+
+	  public static void signUp(CognitoIdentityProviderClient identityProviderClient, 
+	  String clientId, 
+	  String firstName, 
+	  String lastName,
+      String email, 
+	  String password) {
+        AttributeType userAttrs = AttributeType.builder()
+                .name("firstName")
+                .value(firstName)
+				.name("lastName")
+                .value(lastName)
+				.name("email")
+                .value(email)
+                .build();
+
+        List<AttributeType> userAttrsList = new ArrayList<>();
+        userAttrsList.add(userAttrs);
+        try {
+            SignUpRequest signUpRequest = SignUpRequest.builder()
+                    .userAttributes(userAttrsList)
+                    .username(email)
+                    .clientId(clientId)
+                    .password(password)
+                    .build();
+
+            identityProviderClient.signUp(signUpRequest);
+            System.out.println("User has been signed up ");
+
+        } catch (CognitoIdentityProviderException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+    }
+
+	private String getClientId() throws RuntimeException {
+
+		    ListUserPoolsRequest request = ListUserPoolsRequest.builder().
+                    build();
+			String userPoolId = "";
+        	ListUserPoolsResponse response = identityProviderClient.listUserPools(request);
+           	for(UserPoolDescriptionType s :  response.userPools()){
+				System.out.println("user pool name "+s.name());
+				// Try printing USER_POOL as in log user pool id not print
+				if(s.name().equals("cmtr-add4fd60-"+USER_POOL))
+				{
+					userPoolId = s.id();
+					break;
+				}
+			}
+			System.out.println("User pool Id"+userPoolId);
+    return identityProviderClient.listUserPoolClients(ListUserPoolClientsRequest.builder()
+                    .userPoolId(userPoolId)
+                    .maxResults(1)
+                    .build())
+            .userPoolClients().stream()
+            .filter(client -> client.clientName().contains("client-app"))
+            .findAny()
+            .orElseThrow(() -> new RuntimeException("client 'client-app' not found"))
+            .clientId();
+}
 }
