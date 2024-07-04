1f12c0247616c46ff0ac84feb621cc223e6384ec
Prashant Mangate <prashant_mangate@epam.com>
2024-07-04 16:33:48 -0700

    update code

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -7,30 +7,57 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.print.DocFlavor.READER;
-
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
+import com.amazonaws.services.cognitoidp.model.AddCustomAttributesRequest;
+import com.amazonaws.services.cognitoidp.model.AdminConfirmSignUpRequest;
+import com.amazonaws.services.cognitoidp.model.AdminConfirmSignUpResult;
+import com.amazonaws.services.cognitoidp.model.AdminCreateUserRequest;
+import com.amazonaws.services.cognitoidp.model.AdminCreateUserResult;
+import com.amazonaws.services.cognitoidp.model.AdminInitiateAuthRequest;
+import com.amazonaws.services.cognitoidp.model.AdminInitiateAuthResult;
+import com.amazonaws.services.cognitoidp.model.AdminRespondToAuthChallengeRequest;
+import com.amazonaws.services.cognitoidp.model.AdminRespondToAuthChallengeResult;
+import com.amazonaws.services.cognitoidp.model.AdminUpdateUserAttributesRequest;
+import com.amazonaws.services.cognitoidp.model.AttributeType;
+import com.amazonaws.services.cognitoidp.model.AuthFlowType;
+import com.amazonaws.services.cognitoidp.model.ChallengeNameType;
+import com.amazonaws.services.cognitoidp.model.CreateUserPoolClientRequest;
+import com.amazonaws.services.cognitoidp.model.ListUserPoolClientsRequest;
+import com.amazonaws.services.cognitoidp.model.ListUserPoolsRequest;
+import com.amazonaws.services.cognitoidp.model.ListUserPoolsResult;
+import com.amazonaws.services.cognitoidp.model.SignUpRequest;
+import com.amazonaws.services.cognitoidp.model.SignUpResult;
+import com.amazonaws.services.cognitoidp.model.UserPoolDescriptionType;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.stream.JsonReader;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminCreateUserRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.MessageActionType;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.UserPoolDescriptionType;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminCreateUserRequest.Builder;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.CognitoIdentityProviderException;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolClientsRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsRequest;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsResponse;
+// import software.amazon.awssdk.regions.Region;
+// import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminCreateUserRequest;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminCreateUserResponse;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminInitiateAuthRequest;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminInitiateAuthResponse;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminRespondToAuthChallengeRequest;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AdminRespondToAuthChallengeResponse;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.AuthFlowType;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.ChallengeNameType;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.CognitoIdentityProviderException;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.ConfirmSignUpRequest;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolClientsRequest;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsRequest;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoolsResponse;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpResponse;
+// import software.amazon.awssdk.services.cognitoidentityprovider.model.UserPoolDescriptionType;
 
 @LambdaHandler(lambdaName = "api_handler",
 	roleName = "api_handler-role",
@@ -42,19 +69,20 @@ import software.amazon.awssdk.services.cognitoidentityprovider.model.ListUserPoo
         @EnvironmentVariable(key = "region", value = "${region}"),
         @EnvironmentVariable(key = "tables_table", value = "${tables_table}"),
 		@EnvironmentVariable(key = "reservations_table", value = "${reservations_table}"),
-		@EnvironmentVariable(key = "booking_userpool", value = "${booking_userpool}")
+		@EnvironmentVariable(key = "userpool", value = "${booking_userpool}")
 	})
 public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	private static final String USER_POOL = System.getenv("booking_userpool");
-	private static final Region REGION = Region.EU_CENTRAL_1;
+	private static final String USER_POOL = System.getenv("userpool");
+	//private static final Region REGION = ;
 	private final Gson gson = new Gson();
 
-	private CognitoIdentityProviderClient identityProviderClient;
-
+	//private CognitoIdentityProviderClient identityProviderClient;
+	private AWSCognitoIdentityProvider cognitoClient;
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		
-		identityProviderClient = CognitoIdentityProviderClient.builder().region(REGION).build();
+	//	identityProviderClient = CognitoIdentityProviderClient.builder().region(REGION).build();
+	cognitoClient = AWSCognitoIdentityProviderClientBuilder.defaultClient();
 
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		
@@ -62,53 +90,115 @@ public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 		context.getLogger().log(request.toString());
 		LinkedHashMap<String, Object> reqObj = (LinkedHashMap) request;
 
-		/* 
-		/signup POST
-		/signin POST
-		/tables POST
-		/tables GET
-		/reservations POST
-		/reservations GET
-		*/
-
 		if(reqObj.get("resource") instanceof String)
 			rawPath = reqObj.get("resource").toString();
 
 		if(rawPath.contains("/signup")) {// POST
 
 			System.out.println("resource name: "+rawPath);
-			 Builder createUserRequest = AdminCreateUserRequest.builder().messageAction(MessageActionType.SUPPRESS);
 			
-			 // createUserRequest
-			// .username(reqObj.get("email").toString())
-			// .temporaryPassword("Aws1#AwsCloud2#")
-			// .userPoolId(userPoolId);
 			System.out.println(reqObj.get("body").toString());
-
+			System.out.println("System Env user pool "+USER_POOL);	
 			JsonReader reader = new JsonReader(new StringReader(reqObj.get("body").toString()));
 			reader.setLenient(true);
 			User user = gson.fromJson(reader, User.class);
-			System.out.println(user.toString());
-			signUp(identityProviderClient,  
+			System.out.println("before signup "+user.toString());
+			signUp(cognitoClient,  
 					getClientId(), 
 					user.getFirstName(), 
 					user.getLastName(), 
 					user.getEmail(), 
 					user.getPassword());
 
-			identityProviderClient.close();
-			resultMap.put("statusCode", 200);			
+			// Confirm Signup	
+
+			//create user : start
+
+            AdminConfirmSignUpRequest userRequest = new AdminConfirmSignUpRequest()
+					.withUsername(user.getEmail())
+                    .withUserPoolId(getUserPoolId());           
+
+            AdminConfirmSignUpResult response = cognitoClient.adminConfirmSignUp(userRequest);
+
+			AttributeType userAttrs = new AttributeType()
+			.withName("email")
+			.withValue(user.getEmail())
+			.withName("email_verified")
+			.withValue("true")
+			;
+
+			AdminUpdateUserAttributesRequest reqAttr = new AdminUpdateUserAttributesRequest()
+			.withUserAttributes(userAttrs)
+			.withUserPoolId(getUserPoolId())
+			.withUsername(user.getEmail());
+			cognitoClient.adminUpdateUserAttributes(reqAttr);
+
+
+			//create user : ENd
+			System.out.println("user confirmed"+response.getSdkResponseMetadata());
+			System.out.println("after confirmed signup "+user.toString());
+			Map<String,String> initialParams = new HashMap<String,String>();
+            initialParams.put("USERNAME", user.getEmail());
+            initialParams.put("PASSWORD", user.getPassword());
+	
+	
+
+			AdminInitiateAuthRequest authRequest = 
+			new AdminInitiateAuthRequest()
+                    .withAuthFlow(AuthFlowType.ADMIN_NO_SRP_AUTH.name())
+                    .withAuthParameters(initialParams)
+                    .withClientId(getClientId())
+                    .withUserPoolId(getUserPoolId());
+
+
+			AdminInitiateAuthResult initialResponse = null;
+			System.out.println("Client Id"+getClientId()+" user pool id "+getUserPoolId());
+//			try{
+		  			// System.out.print("Confirm signup : admin initiate auth called after signup");
+
+			        //  initialResponse = cognitoClient.adminInitiateAuth(authRequest);
+					//  System.out.println("response after admin auth: "+initialResponse.toString());
+					//  System.out.println("adminInitiateAuth challenge session "+initialResponse.getSession());
+					//  System.out.println("adminInitiateAuth challenge name "+initialResponse.getChallengeName());
+
+					//  Map<String,String> challengeResponses = new HashMap<String,String>();
+					//  challengeResponses.put("USERNAME", user.getEmail());
+					//  challengeResponses.put("PASSWORD", user.getPassword());
+					//  challengeResponses.put("NEW_PASSWORD", user.getPassword());
+ 
+					//  AdminRespondToAuthChallengeRequest finalRequest = new AdminRespondToAuthChallengeRequest()
+					//  .withChallengeName(ChallengeNameType.NEW_PASSWORD_REQUIRED)
+					//  .withChallengeResponses(challengeResponses)
+					//  .withClientId(getClientId())
+					//  .withUserPoolId(getUserPoolId())
+					//  .withSession(initialResponse.getSession());
+ 
+
+					//  AdminRespondToAuthChallengeResult challengeResponse = cognitoClient.adminRespondToAuthChallenge(finalRequest);
+					//  System.out.println("Challenge Response "+challengeResponse.getAuthenticationResult().getAccessToken());
+ 
+					//  cognitoClient.shutdown();
+					//  resultMap.put("statusCode", 200);		
+
+					// }catch(Exception e){
+					// e.printStackTrace();
+					// System.out.println(e.getMessage());
+			//}	
+			cognitoClient.shutdown();
+			resultMap.put("statusCode", 200);	
 		}
-		else if(rawPath.contains("singin")) { // POST
-			System.out.println("resource name: "+rawPath);
-			// POST request will contain : 
-			// {
-			// 	"email": // email
-			// 	"password": // alphanumeric + any of "$%^*", 12+ chars
-			// }
-			String accessTocken="";
+		else if(rawPath.contains("signin")) { // POST
+			
+			System.out.println("resource name: "+rawPath);			
+			JsonReader reader = new JsonReader(new StringReader(reqObj.get("body").toString()));
+			reader.setLenient(true);
+			User user = gson.fromJson(reader, User.class);
+			System.out.println(user.toString());
+
+			AdminInitiateAuthResult response = initiateAuth(cognitoClient,getClientId(),user.getEmail(), user.getPassword(),getUserPoolId());
 			resultMap.put("statusCode", 200);
-			resultMap.put("accessToken",accessTocken);
+			resultMap.put("accessToken",response.getAuthenticationResult().getAccessToken());
+			cognitoClient.shutdown();
 		} 
 		else if(rawPath.contains("tables")) { //GET
 			System.out.println("resource name: "+rawPath);
@@ -207,63 +297,110 @@ public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
 	}
 
-	  public static void signUp(CognitoIdentityProviderClient identityProviderClient, 
+	  public static void signUp(AWSCognitoIdentityProvider identityProviderClient, 
 	  String clientId, 
 	  String firstName, 
 	  String lastName,
       String email, 
 	  String password) {
-        AttributeType userAttrs = AttributeType.builder()
-                .name("firstName")
-                .value(firstName)
-				.name("lastName")
-                .value(lastName)
-				.name("email")
-                .value(email)
-                .build();
+        AttributeType userAttrs = new AttributeType()
+                .withName("firstName")
+                .withValue(firstName)
+				.withName("lastName")
+                .withValue(lastName)
+				.withName("email")
+                .withValue(email);
 
         List<AttributeType> userAttrsList = new ArrayList<>();
         userAttrsList.add(userAttrs);
         try {
-            SignUpRequest signUpRequest = SignUpRequest.builder()
-                    .userAttributes(userAttrsList)
-                    .username(email)
-                    .clientId(clientId)
-                    .password(password)
-                    .build();
+            SignUpRequest signUpRequest = new SignUpRequest()
+                    .withUserAttributes(userAttrsList)
+                    .withUsername(email)
+                    .withClientId(clientId)
+                    .withPassword(password)
+                    ;
 
-            identityProviderClient.signUp(signUpRequest);
+            SignUpResult resp = identityProviderClient.signUp(signUpRequest);
             System.out.println("User has been signed up ");
 
-        } catch (CognitoIdentityProviderException e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
         }
     }
 
-	private String getClientId() throws RuntimeException {
+	private String getUserPoolId(){
 
-		    ListUserPoolsRequest request = ListUserPoolsRequest.builder().
-                    build();
-			String userPoolId = "";
-        	ListUserPoolsResponse response = identityProviderClient.listUserPools(request);
-           	for(UserPoolDescriptionType s :  response.userPools()){
-				System.out.println("user pool name "+s.name());
-				// Try printing USER_POOL as in log user pool id not print
-				if(s.name().equals("cmtr-add4fd60-"+USER_POOL))
-				{
-					userPoolId = s.id();
-					break;
-				}
+		ListUserPoolsRequest request = new ListUserPoolsRequest();
+
+		String userPoolId = "";
+		ListUserPoolsResult response = cognitoClient.listUserPools(request);
+
+		for(UserPoolDescriptionType s :  response.getUserPools()){
+			if(s.getName().equals(USER_POOL))
+			{
+				userPoolId = s.getId();
+				break;
+			}
+			
 			}
 			System.out.println("User pool Id"+userPoolId);
-    return identityProviderClient.listUserPoolClients(ListUserPoolClientsRequest.builder()
-                    .userPoolId(userPoolId)
-                    .maxResults(1)
-                    .build())
-            .userPoolClients().stream()
-            .filter(client -> client.clientName().contains("client-app"))
-            .findAny()
-            .orElseThrow(() -> new RuntimeException("client 'client-app' not found"))
-            .clientId();
+		
+			return userPoolId;
+	}
+
+	private String getClientId() throws RuntimeException {
+		ListUserPoolClientsRequest  req = 
+				new ListUserPoolClientsRequest()
+				.withUserPoolId(getUserPoolId());
+	
+    return cognitoClient.listUserPoolClients(req).getUserPoolClients().stream()
+	         .filter(client -> client.getClientName().contains("client-app"))
+             .findAny()
+             .orElseThrow(() -> new RuntimeException("client 'client-app' not found"))
+             .getClientId();
 }
+	
+	public static AdminInitiateAuthResult initiateAuth(AWSCognitoIdentityProvider identityProviderClient,
+			String clientId, String userName, String password, String userPoolId) {
+			try {
+					Map<String, String> authParameters = new HashMap<>();
+					authParameters.put("USERNAME", userName);
+					authParameters.put("PASSWORD", password);
+
+					AdminInitiateAuthRequest authRequest = new AdminInitiateAuthRequest()
+					.withClientId(clientId)
+					.withUserPoolId(userPoolId)
+					.withAuthParameters(authParameters)
+					.withAuthFlow(AuthFlowType.ADMIN_NO_SRP_AUTH);
+					
+
+					AdminInitiateAuthResult response = identityProviderClient.adminInitiateAuth(authRequest);
+					System.out.println("Result access token is : " + response.getAuthenticationResult().getAccessToken());
+					return response;
+
+			} catch (Exception e) {
+					System.err.println(e.getMessage());
+					System.exit(1);
+			}
+			return null;
+		}
+
+	// public static void confirmSignUp(CognitoIdentityProviderClient identityProviderClient, String clientId, String code,
+    //         String userName) {
+	// 			try {
+	// 			ConfirmSignUpRequest signUpRequest = ConfirmSignUpRequest.builder()
+	// 					.clientId(clientId)
+	// 					.confirmationCode(code)
+	// 					.username(userName)
+	// 					.build();
+
+	// 			identityProviderClient.confirmSignUp(signUpRequest);
+	// 			System.out.println(userName + " was confirmed");
+
+	// 		} catch (CognitoIdentityProviderException e) {
+	// 			System.err.println(e.awsErrorDetails().errorMessage());
+	// 			System.exit(1);
+	// 		}
+	// 	}
 }
